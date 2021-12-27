2f56f2fc55cd1b7205c24c54f8278af124302a15
diefesson <diefesson.so@gmail.com>
2021-12-27 08:48:52 -0300

    refactor: data dont need to implement cloneable for evolve

--- src/ai/genetic/evolve.rs
+++ src/ai/genetic/evolve.rs
@@ -1,25 +1,18 @@
-use std::iter::repeat;
-
-pub fn evolve<C, Sc, Se, Cr, M>(
-    population: Vec<C>,
+pub fn evolve<S, Sc, Se, Cr, M>(
+    mut population: Vec<S>,
     scorer: &Sc,
     selector: &Se,
     crosser: &Cr,
     mutator: &M,
     target_size: usize,
     generations: usize,
-) -> (Vec<C>, Vec<f64>)
+) -> (Vec<S>, Vec<f64>)
 where
-    C: Clone,
-    Sc: Fn(&C) -> f64,
+    Sc: Fn(&S) -> f64,
     Se: Fn(&Vec<f64>) -> usize,
-    Cr: Fn(&C, &C) -> C,
-    M: Fn(&C) -> C,
+    Cr: Fn(&S, &S) -> S,
+    M: Fn(&S) -> S,
 {
-    let mut population = repeat(population)
-        .flatten()
-        .take(target_size)
-        .collect::<Vec<_>>();
     let mut scores = population.iter().map(|s| scorer(s)).collect::<Vec<_>>();
     for _ in 0..generations {
         let mut offspring = Vec::with_capacity(population.len());
