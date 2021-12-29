25a9e793284a8662647b5c85cfd693018d674f97
diefesson <diefesson.so@gmail.com>
2021-12-29 11:19:33 -0300

    feat: mse x epoch graph

---
+++ tools/msexepochgraph.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plot
+
+_DATA_PATH = "output/mse-x-epoch.csv"
+_OUTPUT_PATH = "output/mse-x-epoch.png"
+
+
+def main():
+    data = pd.read_csv(_DATA_PATH)
+    xs, ys = data.iloc[:, 0], data.iloc[:, 1]
+
+    plot.xlabel("Epoch")
+    plot.ylabel("Mean square error")
+    plot.plot(xs, ys)
+    plot.legend()
+    plot.savefig(_OUTPUT_PATH)
+
+
+if __name__ == "__main__":
+    main()
