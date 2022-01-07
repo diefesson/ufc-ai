705d5c849d0f56c52dfdffca9f54ce699cc36f36
diefesson <diefesson.so@gmail.com>
2022-01-07 14:38:22 -0300

    chore: removed plotter dependecy

--- src/demo/gradient/mod.rs
+++ src/demo/gradient/mod.rs
@@ -3,11 +3,9 @@ mod demo1;
 mod demo2;
 mod demo2ls;
 mod demo3;
-mod plotterstest;
 
 pub use csvtest::*;
 pub use demo1::*;
 pub use demo2::*;
 pub use demo2ls::*;
 pub use demo3::*;
-pub use plotterstest::*;
